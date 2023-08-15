1a311ea442170739ea4cfb7ad4ec3e6db6e12062
“ilyaxvgreat” <ilyaxvgreat@gmail.com>
2023-08-15 14:43:10 +0300

    test ilia

--- src/main/java/com/khomchenko/proselyteapi/ProselyteapiApplication.java
+++ src/main/java/com/khomchenko/proselyteapi/ProselyteapiApplication.java
@@ -7,8 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 public class ProselyteapiApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(ProselyteapiApplication.class, args);
 	}
+
 
 }
