abe6b05db439f518d13fef85ee3ad961e67970c3
Ilya.Khomchenko <ilya.khomchenko@logicomsoft.com>
2023-08-26 11:39:37 +0300

    added HealthRestControllerV1

--- src/main/java/com/khomchenko/proselyteapi/controllers/HealthRestControllerV1.java
+++ src/main/java/com/khomchenko/proselyteapi/controllers/HealthRestControllerV1.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 @RestController
-@RequestMapping
+@RequestMapping("/v1/health")
 public class HealthRestControllerV1 {
 
     @GetMapping
