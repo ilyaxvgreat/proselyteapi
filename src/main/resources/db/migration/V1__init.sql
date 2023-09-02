8232acd3610e91d65dad9d837b14e86891c0318e
Ilya.Khomchenko <ilya.khomchenko@logicomsoft.com>
2023-09-02 13:32:53 +0300

    added flyway

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,5 +1,24 @@
-CREATE TABLE IF NOT EXISTS accounts(
-    id BIGINT NOT NULL PRIMARY KEY ,
-    username VARCHAR(36) NOT NULL ,
-    password VARCHAR(200) NOT NULL
+DROP TABLE IF EXISTS accounts;
+DROP TABLE IF EXISTS companies;
+DROP TABLE IF EXISTS stocks;
+CREATE TABLE accounts
+(
+    id       SERIAL PRIMARY KEY,
+    username VARCHAR(36) NOT NULL,
+    password VARCHAR(36) NOT NULL,
+    api_key  VARCHAR(36) NOT NULL
 );
+CREATE TABLE companies
+(
+    id   SERIAL PRIMARY KEY,
+    name VARCHAR(36) NOT NULL,
+    code VARCHAR(36) NOT NULL
+);
+CREATE TABLE stocks
+(
+    id           SERIAL PRIMARY KEY,
+    price        INT         NOT NULL,
+    created_date TIMESTAMP   NOT NULL DEFAULT current_timestamp,
+    code         VARCHAR(36) NOT NULL
+);
+
